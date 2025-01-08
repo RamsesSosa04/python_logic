cdad11e1bdd2ff11387e62c74ed316eff88f5209
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-07 19:33:03 -0700

    Primer ejercicio

---
+++ Python/capital_letters.py
@@ -0,0 +1,15 @@
+#Write a function to convert a name into initials. This kata strictly takes two words with one space in 
+#between them.
+#The output should be two capital letters with a dot separating them.
+#It should look like this:
+#Sam Harris => S.H
+#patrick feeney => P.F
+
+def obtener_nombre(nombre):
+    nombre, apellido = nombre.split()
+ 
+    iniciales = f"{nombre[0].upper()}.{apellido[0].upper()}."
+    return iniciales
+
+print(obtener_nombre("Ramses Sosa"))    
+    
