0c94aa6ffb4a4392a6802fc76417843041426b3c
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-09 23:09:21 -0700

    basic operations exercise

--- Python/basic_op.py
+++ Python/basic_op.py
@@ -6,5 +6,13 @@ def basic_op(operator, value1, value2):
     elif operator == '*':
         return value1 * value2
     elif operator == '/':
-        return value1 / value2
+        if value2 == '0':
+            return 'Error, division por cero'
+        else: 
+            return value1 / value2
+    else: 
+        return 'Valor invalido'
 print(basic_op('+', 10, 5))
+print(basic_op('-', 10, 7))
+print(basic_op('*', 10, 7))
+print(basic_op('/', 100, 5))
