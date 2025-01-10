bd2af974b357456973581c5d29870940645095a7
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-10 08:55:52 -0700

    goals fuction exercise

--- Python/goals.py
+++ Python/goals.py
@@ -1,3 +1,14 @@
+"""
+Cristiano Ronaldo is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+"""
+
 def goals(laLiga, copaDelRey, championsLeague):
     return laLiga + copaDelRey + championsLeague
 
