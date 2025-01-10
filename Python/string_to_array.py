9d4cf0d0f16ad5d295b228698a9597cd048ee15e
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-09 23:26:59 -0700

    String to array exercise

--- Python/string_to_array.py
+++ Python/string_to_array.py
@@ -1,3 +1,12 @@
+"""
+  Divide una cadena en una lista de palabras.
+
+  Args:
+    cadena: La cadena de texto a dividir.
+
+  Returns:
+    Una lista con las palabras de la cadena.
+  """
 def string_to_array(s):
     array = s.split(' ')
     return array
