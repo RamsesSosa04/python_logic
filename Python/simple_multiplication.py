a06d1a0e856f8b3f3202044400222b7d40945119
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-12 16:08:53 -0700

    Multiplication exercise

--- Python/simple_multiplication.py
+++ Python/simple_multiplication.py
@@ -1,3 +1,12 @@
+"""
+  Multiplica un número por 8 si es par y por 9 si es impar.
+
+  Args:
+    number: El número a multiplicar.
+
+  Returns:
+    El resultado de la multiplicación.
+"""
 def simple_multiplication(number) :
     if number % 2 == 0:
         return number * 8
