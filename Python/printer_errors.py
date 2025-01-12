06f6f32d400c838557e572761ffbd6b97cad5273
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-12 16:28:53 -0700

    Multiplication exercise

---
+++ Python/printer_errors.py
@@ -0,0 +1,8 @@
+def printer_error(s):
+    valid_chars = 'abcdefghijklm'
+    errors = 0
+    for char in s:
+        if char not in valid_chars:
+            errors += 1
+    return f"{errors} / {len(s)}"
+print(printer_error("aaaxbbbbyyhwawiwjjjwwm"))
