7ca57686e4f8fb49b0f299cc9e229408dbc484c2
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-13 23:29:07 -0700

    String error exercise

--- Python/String_error.py
+++ Python/String_error.py
@@ -1,10 +1,10 @@
-def multiplicar_y_sumar(valor):
-  try:
-    resultado = int(valor) * 50 + 6
-    return resultado
-  except ValueError:
-    return "Error"
+def multiplicar_y_sumar(a):
+    try:
+        resultado = int(a) * 50 + 6
+        return resultado
+    except ValueError:
+        return "error"
 
 # Ejemplos de uso:
-print(multiplicar_y_sumar(3))  # Imprime: 156
+print(multiplicar_y_sumar(50))  # Imprime: 156
 print(multiplicar_y_sumar("hola"))  # Imprime: Error
