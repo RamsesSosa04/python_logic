b0b858f68d8eec536eebec05a266484748d2f78b
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-14 23:36:36 -0700

    Number to expended form exercise

--- Python/NumberExpanded.py
+++ Python/NumberExpanded.py
@@ -1,5 +1,9 @@
 def expanded_form(num):
-    num_str = str(num)
-    enumerate = []
-    for i, digit in enumerate(num_str[::-1]):
-        
+  num_str = str(num)
+  expanded = []
+  for i, digit in enumerate(num_str[::-1]):  # Iterar de derecha a izquierda
+    value = int(digit) * 10**i
+    if value != 0:
+      expanded.append(str(value))
+  return ' + '.join(reversed(expanded))
+print(expanded_form(70304))
