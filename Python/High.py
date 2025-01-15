00899c80b87a94423268e549bdd75ae1d5bf4782
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-15 13:21:31 -0700

    Most valuable word exercise

---
+++ Python/High.py
@@ -0,0 +1,19 @@
+def high(x):
+    puntuaciones = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,
+                 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+                 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
+                 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+    words = x.split()
+    max_score = 0
+    word = words[0]
+    
+    for w in words:
+        score = sum(puntuaciones[c] for c in word)
+        if score > max_score:
+            score = max_score
+            word = w
+        elif score == max_score:
+            if words.index(w) < words.index(word):
+                word = w
+    return w
+print(high("hola hola"))
