4c4a17a39cf0616a85199fba96da99ecc01d4bda
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-15 23:26:04 -0700

    URL domain exercise

--- Python/url.py
+++ Python/url.py
@@ -1,5 +1,20 @@
-def domain_name(url):
-    partesUrl = url.split('/')
-    return partesUrl
+from urllib.parse import urlparse
 
-print(domain_name('https://github.com/RamsesSosa04'))
+def extraer_dominio(url):
+
+  # Analiza la URL utilizando urllib.parse
+  parsed_uri = urlparse(url)
+  # Extrae el netloc (nombre de red), que incluye el subdominio y el dominio
+  result = parsed_uri.netloc
+  # Elimina el subdominio si existe (por ejemplo, 'www.')
+  result = result.split('.')[-2]
+  return result
+
+# Ejemplos de uso:
+url1 = "http://github.com/carbonfive/raygun"
+url2 = "http://www.zombie-bites.com"
+url3 = "https://www.cnet.com"
+
+print(extraer_dominio(url1))  # Salida: github
+print(extraer_dominio(url2))  # Salida: zombie-bites
+print(extraer_dominio(url3))  # Salida: cnet
