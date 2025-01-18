0882c1d00b87000289e2e00dd04373554ef3138b
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-17 18:00:49 -0700

    Maximum and minimum numbers exercise

---
+++ Python/max_min.py
@@ -0,0 +1,17 @@
+def minimum(arr):
+    min_value = arr[0]
+    for num in arr[1:]:
+        if num < min_value:
+            min_value = num
+    return min_value
+
+def maximum(arr):
+    max_value = arr[0]
+    for num in arr[1:]:
+        if num > max_value:
+            max_value = num
+    return max_value
+list = [4, 6, 2, 1, 9, 63, -134, 566]
+min_value = minimum(list)
+max_value = maximum(list)
+print(f"Min: {min_value}, Max: {max_value}")
