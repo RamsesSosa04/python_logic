b666830b3583354b3ea48ad6bca25d6c17fca899
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-18 23:42:23 -0700

    Sum of positives exercise

--- Python/sum_positive.py
+++ Python/sum_positive.py
@@ -1,4 +1,5 @@
- """
+def sumar_positivos(numeros):
+  """
   Suma todos los números positivos en una lista.
 
   Args:
@@ -7,3 +8,14 @@
   Returns:
     La suma de los números positivos, o 0 si no hay ninguno.
   """
+
+  suma = 0
+  for numero in numeros:
+    if numero > 0:
+      suma += numero
+  return suma
+
+# Ejemplo de uso:
+lista_numeros = [1, -4, 7, 12]
+resultado = sumar_positivos(lista_numeros)
+print(resultado)  # Imprime: 20
