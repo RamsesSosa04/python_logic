c986c2d7bfc1d4224a56ad51f6674882bf5c74b9
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-19 15:35:46 -0700

    Are you playing banjo? exercise

--- Python/playing_banjo.py
+++ Python/playing_banjo.py
@@ -1,3 +1,13 @@
+"""
+  Checks if a person plays banjo based on their name.
+
+  Args:
+    name: The person's name.
+
+  Returns:
+    A string indicating whether the person plays banjo.
+"""
+
 def areYouPlayingBanjo(name):
     if name.lower().startswith('r'):
         return f"{name} plays banjo"
