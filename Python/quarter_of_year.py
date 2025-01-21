cb9526b9052213539efad8801148b2c8a093f79e
Ramsés Sosa <sosaramses08@gmail.com>
2025-01-21 13:57:04 -0700

    Quarter of the year exercise

--- Python/quarter_of_year.py
+++ Python/quarter_of_year.py
@@ -1,2 +1,10 @@
-def quarter_of(year):
-    pass
+def quarter_of(month):
+    if month < 3 and month > 0:
+        return 1
+    elif month < 3 and month > 7:
+        return 2
+    elif month < 7 and month > 10:
+        return 3
+    else:
+        return 4
+print(quarter_of(11))
